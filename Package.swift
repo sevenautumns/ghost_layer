a22128d88c22f6a164b8f6aa561537a0f427ae79
Autumnal <github@p.autumnal.de>
2026-04-20 18:43:12 +0200

    chore: rename repo to PascalCase

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GhostLayer",
-            url: "https://github.com/sevenautumns/ghost_layer/releases/download/v0.0.5/GhostLayer.xcframework.zip",
+            url: "https://github.com/sevenautumns/GhostLayer/releases/download/v0.0.5/GhostLayer.xcframework.zip",
             checksum: "a0199f144e40211b1a32e87cb5db29caa0a893ad467bea128597d845b53c22f0"
         )
     ]
